142c1a077d72022f8c2b7aff8ae65a79dd5f60f4
bigdot123456 <bigdot@126.com>
2020-02-21 17:42:26 +0800

    add log and spring boot demo

---
+++ src/main/java/com/run/dw/DwApplication.java
@@ -0,0 +1,31 @@
+package com.run.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author liqinghua
+ */
+@SpringBootApplication
+@RestController
+public class DwApplication {
+    private static final Logger logger = LoggerFactory.getLogger(DwApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("This is a check message!");
+        logger.warn("goo!");
+        logger.error("this is a error message");
+
+        SpringApplication.run(DwApplication.class, args);
+    }
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World from Tomcat";
+    }
+
+}
