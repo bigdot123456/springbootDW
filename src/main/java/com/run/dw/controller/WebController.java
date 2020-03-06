93c6fb7f98ed762e64d3a4be9d2b024c8b9b99b1
bigdot123456 <bigdot@126.com>
2020-03-06 10:34:07 +0800

    add internation files

--- src/main/java/com/run/dw/controller/WebController.java
+++ src/main/java/com/run/dw/controller/WebController.java
@@ -9,4 +9,18 @@ public class WebController {
     public String index() {
         return "index";
     }
+    @RequestMapping("/view-products")
+    public String viewProducts() {
+        return "view-products";
+    }
+    @RequestMapping("/add-products")
+    public String addProducts() {
+        return "add-products";
+    }
+    @RequestMapping("/locale")
+    public String locale() {
+        return "locale";
+    }
+
+
 }
